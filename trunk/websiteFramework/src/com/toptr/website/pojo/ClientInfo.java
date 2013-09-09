c5620452be1fca4e2a4c9d0606a6e861e3f1c40e
trisection85@gmail.com <trisection85@gmail.com@bea2474e-6a2b-ff62-820e-0e2c597f8db9>
2013-09-09 15:21:35 +0000

    malei：提交建库脚本、提交其他以来jar、调整了配置文件

--- trunk/websiteFramework/src/com/toptr/website/pojo/ClientInfo.java
+++ trunk/websiteFramework/src/com/toptr/website/pojo/ClientInfo.java
@@ -12,23 +12,95 @@ import javax.persistence.Table;
 import org.apache.commons.lang.builder.ToStringBuilder;
 
 @Entity
-@Table(name="CLIENTINFO")
+@Table(name="CLIENT_INFO")
 public class ClientInfo
 {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
-    @Column private String clientcode;
-    @Column private String clientname;
-    @Column private String status;
-    @Column private Date inputdate;
-    @Column private String email;
-    @Column private String phone;
+    @Column 
+    private String clientcode;
+    @Column 
+    private String clientname;
+    @Column 
+    private String status;
+    @Column 
+    private Date inputdate;
+    @Column 
+    private String email;
+    @Column 
+    private String phone;
+    @Column 
+    private String comment;
     
-    @Override
+    public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getClientcode() {
+		return clientcode;
+	}
+
+	public void setClientcode(String clientcode) {
+		this.clientcode = clientcode;
+	}
+
+	public String getClientname() {
+		return clientname;
+	}
+
+	public void setClientname(String clientname) {
+		this.clientname = clientname;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Date getInputdate() {
+		return inputdate;
+	}
+
+	public void setInputdate(Date inputdate) {
+		this.inputdate = inputdate;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	@Override
     public String toString()
     {
         return ToStringBuilder.reflectionToString(this);
     }
-    //setters & getters    
+	
 }
